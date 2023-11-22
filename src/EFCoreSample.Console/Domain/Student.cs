8aed453c8f1f80df94c8ab9dda239b0ea7f945a2
Walter Casella <walter.casella@gruposancristobal.com>
2023-11-22 16:01:44 -0300

    Add CreatedTimestamp to Student entity

--- src/EFCoreSample.Console/Domain/Student.cs
+++ src/EFCoreSample.Console/Domain/Student.cs
@@ -10,5 +10,6 @@
         public string Email { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string ZipCode { get; set; } = string.Empty;
+        public DateTime CreatedTimestamp { get; set; }
     }
 }
